80417ad90aa754a8f15ef1a8dc946b74a81860fe
noemipintaldi <noemipintaldi@gmail.com>
2022-03-16 20:17:15 +0100

    break

--- JSnack 4/myscript.js
+++ JSnack 4/myscript.js
@@ -1,19 +1,19 @@
-//Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
-
 //Crea due array che hanno un numero di elementi diversi
 
 let array1 = [1,2, 3, 4];
 
 let array2 = [1,2, 3, 4, 5, 6];
 
-
+//Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
 for (let index = 0; index < array2.length; index++) {
     
     console.log(array1);
     console.log(array2);
-    
+
     if (array1.length != array2.length) {
         array1.push("item");
+    }else{
+        break;
     }
 
 }
