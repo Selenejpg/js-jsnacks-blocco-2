a2ed8fb944fae466c7ae0a76325b12c2d6e6adcc
noemipintaldi <noemipintaldi@gmail.com>
2022-03-16 17:31:06 +0100

    push fino ad ora

---
+++ JSnack 2/myscript.js
@@ -0,0 +1,7 @@
+let nomi = ["Piero", "Giovanni", "Gertrude", "Amilcare", "Alfonso"];
+
+let cognomi = ["Rossi", "Alighieri", "Verga", "Leopardi", "Manzoni"];
+
+let listaFalsa = [nomi + cognomi]
+
+let falsi = document.getElementById("nomiFalsi").innerHTML = listaFalsa;
