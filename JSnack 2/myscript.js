fee89adc8e56a481dadccf4ab6ad49db7069216c
noemipintaldi <noemipintaldi@gmail.com>
2022-03-16 19:20:32 +0100

    finished

--- JSnack 2/myscript.js
+++ JSnack 2/myscript.js
@@ -2,6 +2,13 @@ let nomi = ["Piero", "Giovanni", "Gertrude", "Amilcare", "Alfonso"];
 
 let cognomi = ["Rossi", "Alighieri", "Verga", "Leopardi", "Manzoni"];
 
-let listaFalsa = [nomi + cognomi]
+let combinedArray = []
 
-let falsi = document.getElementById("nomiFalsi").innerHTML = listaFalsa;
+for (let i = 0; i < 5; i++) { 
+    let nomeRandom = nomi[Math.floor(Math.random()*nomi.length)];
+    let cognomeRandom = cognomi[Math.floor(Math.random()*cognomi.length)];
+    combinedArray.push( nomeRandom + cognomeRandom )
+    console.log(cognomi.length)
+}
+
+let falsi = document.getElementById("nomiFalsi").innerHTML = combinedArray;
